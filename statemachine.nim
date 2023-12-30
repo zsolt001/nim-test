9f0e9bebe15d7cd839b7c01af7f2aa10c42f85bc
zsolt <zsoltxyz+zsolt001@gmail.com>
2023-12-30 13:31:35 -0500

    better types

--- statemachine.nim
+++ statemachine.nim
@@ -1,82 +1,68 @@
 import std/options
 import std/tables
-import std/hashes
 import pretty
 
 type 
-  StateMachine = object
-    states: seq[State]
-    events: seq[Event]
-    transitionMap: TransitionMap
-
-  State = object of RootObj
-    name: string
-
-  Event = object of RootObj
-    name: string
-  
-  TransitionKey = object
-    currentState: State
-    receivedEvent: Event
-
-  TransitionMap = Table[ TransitionKey, Transition]
-
-  Context = object of RootObj
-    currentState: State
+  Transition[S] = object
+    source: S
+    eventType: string
+    target: S
+    action: Action[S]
 
   Output = object of RootObj
     msg: string
-  
-  Transition = tuple[source: State, event: Event, target: State, action: Action]
-  Action = proc(ctx: Context): Option[Output]
+  Action[S] = proc(ctx: Context[S]): Option[Output]
+
+  StateMachine[S: enum] = object
+    events: seq[string]
+    transitionMap: TransitionMap[S]
+
+  TransitionKey[S] = object
+    state: S
+    eventType: string
+
+  TransitionMap[S] = Table[ TransitionKey[S], Transition[S]]
 
 
-proc `$`(st: State): string =
-  return "State: " & st.name
+  Context[S] = object of RootObj
+    currentState: S
 
-proc `$`(ev: Event): string =
-  return "Event: " & ev.name
-
+ 
 proc `$`(tr: Transition): string =
   return $tr.source & " -> " & $tr.target
 
-# proc hash(k : TransitionKey) : Hash =
-#   result = string(k.currentState).hash !& string(k.receivedEvent).hash
-#   result = !$result
 
 
-func newStateMachine(states: seq[State], events: seq[Event], transitions: seq[Transition]): StateMachine =
-  result.states = states
+func newStateMachine[S:enum](events: seq[string], transitions: seq[Transition]): StateMachine[S] =
   result.events = events
 
   # map the FSM's state and the incoming event to a transition that we need to apply
-  var trmap : TransitionMap
+  var 
+    trmap : TransitionMap[S]
+    key : TransitionKey[S]
 
   for tr in transitions:
-    var key : TransitionKey
-    key.currentState = tr.source
-    key.receivedEvent = tr.event
+    key.state = tr.source
+    key.eventType = tr.eventType
     trmap[key] = tr
 
   result.transitionMap = trmap
 
-proc receive(sm: StateMachine, ctx: var Context, event: Event) : Option[Output] = 
+proc receive[S](sm: StateMachine[S], ctx: var Context[S], event: object) : Option[Output] = 
   print "------------------"
-  echo "<<< ", ctx
-  echo "<<< ", event
-  let transitionKey = TransitionKey(currentState: ctx.currentState, receivedEvent: event)
+  print "<<< ", ctx
+  print "<<< ", event
+  let transitionKey = TransitionKey[S](state: ctx.currentState, eventType: $typeof(event))
   
   try:
     let transition = sm.transitionMap[transitionKey]
-    echo ">>> ", transition
+    print ">>> ", transition
 
     let output = transition.action(ctx)
     ctx.currentState = transition.target
     return output
   except KeyError:
     echo ">>> no transition exists"
-
-
 
 # ============================================================
 #   Create test StateMachine instance and execute some 
@@ -85,46 +71,52 @@ proc receive(sm: StateMachine, ctx: var Context, event: Event) : Option[Output] 
 
 
 type 
-  SmtpCtx = object of Context
+  # Context
+  SmtpCtx[S] = object of Context[S]
     rawLine: string
 
-let 
   # States
-  IdleState = State(name: "idle")
-  ConnectedState = State(name: "connected")
-  ReceivedMailState= State(name: "receivedMail")
-  states = @[IdleState, ConnectedState, ReceivedMailState]
+  State = enum
+    Idle, Connected, ReceivedMail
 
   # Events
-  ConnectEvent = Event(name: "CONNECT")
-  MailCmd = Event(name: "MAIL")
-  QuitCmd =  Event(name: "QUIT")
-  events = @[ConnectEvent, MailCmd, QuitCmd]
+  ConnectEvent = object 
+    clientAddress: string
 
-  # Actions
-  defaultAction : Action = proc(ctx: Context) : Option[Output] =
-    echo ">>> executing default action in state: ", ctx.currentState
-  
+  MailCmd = object 
+    fromArg: string
+
+  QuitCmd = object 
+
+
+var
+  defaultAction : Action[State] = proc(ctx:Context[State]) : Option[Output]  =
+    echo "hello"
+
+let 
+  eventTypes = @[$ConnectEvent, $MailCmd, $QuitCmd]
+
   # Transitions
-  transitions:seq[Transition] = @[
+  transitions = @[
     # Client Connect
-    (IdleState, ConnectEvent, ConnectedState, defaultAction),
+    Transition[State](source: State.Idle, eventType: $ConnectEvent, target: State.Connected, action: defaultAction),
     # MAIL FROM
-    (ConnectedState, MailCmd, ReceivedMailState, defaultAction),
+    Transition[State](source: State.Connected, eventType: $MailCmd, target: State.ReceivedMail, action: defaultAction),
+
     # QUIT
-    (ConnectedState, QuitCmd, IdleState, defaultAction),
-    (ReceivedMailState, QuitCmd, IdleState, defaultAction),
+    Transition[State](source: State.Connected, eventType: $QuitCmd, target: State.Idle, action: defaultAction),
+    Transition[State](source: State.ReceivedMail, eventType: $QuitCmd, target: State.Idle, action: defaultAction),
   ]
  
   
 var
-  fsm = newStateMachine(states, events, transitions)
-  ctx = SmtpCtx(currentState: IdleState)
+  fsm = newStateMachine[State](eventTypes, transitions)
+  ctx = SmtpCtx[State](currentState: State.Idle)
 
 print "\nStarted State Machine\n"
 
-discard fsm.receive(ctx, MailCmd)
-discard fsm.receive(ctx, ConnectEvent)
-discard fsm.receive(ctx, QuitCmd)
+discard fsm.receive(ctx, MailCmd())
+discard fsm.receive(ctx, ConnectEvent())
+discard fsm.receive(ctx, QuitCmd())
 
 
